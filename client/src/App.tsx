37c295a37ebe0bfc5629e78bdae86ff575dcdfe6
Hwang Jung Hyeon <1004wipi@gmail.com>
2023-01-04 12:16:50 +0900

    feat: 회원가입 구현

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from "./pages/sign-up/SignUp";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/sign-up/SignUp.tsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface inputType {
+  email: string;
+  password: string;
+}
+
+const SignUpForm = () => {
+  const [isValid, setIsvalid] = useState<boolean>(true);
+  const [inputs, setInputs] = useState<inputType>({
+    email: "",
+    password: "",
+  });
+  const { email, password } = inputs;
+  const fetchLogin = () => {
+    axios
+      .post("http://localhost:8080/users/login", {
+        email: inputs.email,
+        password: inputs.password,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+  const checkInput = () => {
+    console.log("실행됨");
+    const regex = /[\w\-\.]+\@[\w\-\.]+/;
+    if (password.length >= 8 && regex.test(email)) {
+      setIsvalid(false);
+    } else setIsvalid(true);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  useEffect(() => {
+    checkInput();
+  }, [inputs]);
+
+  return (
+    <>
+      <input name="email" type="text" placeholder="email" onChange={onChange} />
+      <input
+        name="password"
+        type="text"
+        placeholder="password"
+        onChange={onChange}
+      />
+      <button onClick={fetchLogin} disabled={isValid}>
+        회원가입
+      </button>
+      email: {email}
+      pw: {password}
+    </>
+  );
+};
+
+export const SignUp = () => {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  );
+};
