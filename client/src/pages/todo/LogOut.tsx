9effca0041a1bc7d2872280651b9f2c962f4c0b5
Hwang Jung Hyeon <1004wipi@gmail.com>
2023-01-17 18:31:56 +0900

    fix: 로그아웃 버튼 작동하지 않던 에러 해결

--- client/src/pages/todo/LogOut.tsx
+++ client/src/pages/todo/LogOut.tsx
@@ -1,14 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { ERROR } from "../../utils/constant";
 
-const logOutHandler = () => {
-  const navigate = useNavigate();
-  localStorage.removeItem("token");
-  alert(ERROR.LOGIN_REQUIRED_MESSAGE);
-  navigate("/auth/login");
-};
-
 export const LogOut = () => {
+  const logOutHandler = () => {
+    const navigate = useNavigate();
+    localStorage.removeItem("token");
+    alert(ERROR.LOGIN_REQUIRED_MESSAGE);
+    navigate("/auth/login");
+  };
+
   return (
     <>
       <button onClick={logOutHandler}>Logout</button>
